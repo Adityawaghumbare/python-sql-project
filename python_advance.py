6ce002123d7c5704320ff0b79c61e314fede2d31
Adityawaghumbare <adityawaghumbare26@gmail.com>
2026-08-29 17:10:28 +0000

    Day 2 : Python Practice

--- python_advance.py
+++ python_advance.py
@@ -24,9 +24,52 @@
 # average_marks = total_marks / len(students)
 # print(" Total Marks : ", total_marks,"/", len(students*100), "\n","Average Marks : ",  average_marks)
 
-s = "ABCD EF"
-s1 = "H" + s[1:]                  
-s2 = s.replace("ABC", "abc")  
-print(s)
-print(s1)
-print(s2)
+# s = "ABCD EF"
+# s1 = "H" + s[1:]                  
+# s2 = s.replace("ABC", "abc")  
+# print(s)
+# print(s1)
+# print(s2)
+
+# name = input("Enter your name : ")
+# age = int(input("Enter your age : "))
+# s = "My name is {} and I am {} years old.".format( name, age)
+# print(s)
+
+# 1. Create a list of 5 students
+# 2. Store name + marks using dictionaries
+# 3. Print all students
+# 4. Find highest marks
+# 5. Find lowest marks
+# 6. Calculate average
+
+
+students = [                                       # -- Creation of list of 5-6 students
+    {"Name" : "Aditya" , "Marks" : 90 },
+    {"Name" : "Kapil" , "Marks" : 75 },
+    {"Name" : "Yash" , "Marks" : 80 },
+    {"Name" : "Anuj" , "Marks" : 65 },
+    {"Name" : "Atharva" , "Marks" : 70 },
+    {"Name" : "Pranav" , "Marks" : 90 }
+]                                                     # -- Store name + marks using dictionaries
+
+
+for s in students :                                    # -- Print all students name and marks
+    print(f"Name : {s["Name"]}  |  Marks : {s["Marks"]}")
+print(" ")
+
+highest = max(s["Marks"] for s in students)           # -- Find highest marks
+print(f"Highest Marks : {highest} \n")
+
+lowest = min(s["Marks"] for s in students)            # -- Find lowest marks
+print(f"Lowest Marks : {lowest} \n")
+
+marks = []
+for s in students:
+    marks.append(s["Marks"])
+
+total = sum(marks)
+print(f"Sum = {total}\n")
+
+avg = total/(len(marks))                          # -- Calculate average
+print(f"Average : {avg}")
