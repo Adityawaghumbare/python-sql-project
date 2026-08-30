72a1ccb56cc7da48456e86a642c491ee9b78b742
Adityawaghumbare <adityawaghumbare26@gmail.com>
2026-08-30 13:04:26 +0000

    Day 3 : type conversion + basic validation

---
+++ csv_to_sqlite.py
@@ -0,0 +1,18 @@
+import csv
+
+students = []
+
+with open('students.csv','r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+      row["marks"] = int(row["marks"])
+      row["attendance"] = int(row["attendance"])
+      students.append(row)
+      print(row)
+
+
+print(students[0]["marks"] + 15)
+
+for s in students:
+    if s["marks"] > 100 or s["marks"] < 0 :
+        print(f"Invalid marks for {s["name"]}")
